795274a2de9444c1b952f8332e85004ad4fada9f
santanderelias <cazagb@gmail.com>
2025-11-09 21:00:21 -0300

    small tweaks

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const varsVisibilityTable = document.getElementById('varsTable');
 const addTransactionFormBtn = document.getElementById('addTransactionFormBtn');
 const addTransactionTable = document.getElementById('addTransactionTable');
 const amountInputBox = document.getElementById('amountInputBox');
+const statsVisibilityTable = document.getElementById('statisticsTable')
 const transactionDate = document.getElementById('transaction-date');
 const inputGroupSelect01 = document.getElementById('inputGroupSelect01');
 //const rangeValue = document.getElementById('rangeValue');
@@ -14,6 +15,7 @@ let transactionsArr = [];
 let transactionHistoryTableState = 0;
 let varsTableState = 0;
 let addTransactionTableState = 0;
+let statsTableState = 0;
 const now = new Date();
 // --- Ensures the date is based on the local Buenos Aires time ---
 const year = now.getFullYear();
@@ -61,9 +63,11 @@ function transactionHistoryVisibility() {
         transactionHistoryVisibilityTable.style.display = 'table';
         addTransactionTable.style.display = 'none';
         varsVisibilityTable.style.display = 'none';
+        statsVisibilityTable.style.display = 'none';
         transactionHistoryTableState = 1;
         varsTableState = 0;
         addTransactionTableState = 0;
+        statsTableState = 0;
         renderTransactions();
         console.log('ran 1')
     }
@@ -80,7 +84,28 @@ function varsVisibility() {
         varsVisibilityTable.style.display = 'table';
         transactionHistoryVisibilityTable.style.display = 'none';
         addTransactionTable.style.display = 'none';
+        statsVisibilityTable.style.display = 'none';
         varsTableState = 1;
+        transactionHistoryTableState = 0;
+        addTransactionTableState = 0;
+        statsTableState = 0;
+        console.log('ran 1')
+    }
+/*   else {
+        transactionHistoryVisibilityTable.style.display = 'none';
+        varsVisibilityTable.style.display = 'none';
+        varsTableState = 0;
+        console.log('ran 2')
+    }*/
+}
+function statsVisibility() {
+    if (statsTableState == 0){
+        addTransactionTable.style.display = 'none';
+        transactionHistoryVisibilityTable.style.display = 'none';
+        varsVisibilityTable.style.display = 'none';
+        statsVisibilityTable.style.display = 'table';
+        statsTableState = 1;
+        varsTableState = 0;
         transactionHistoryTableState = 0;
         addTransactionTableState = 0;
         console.log('ran 1')
@@ -97,9 +122,11 @@ function newTransactionFormDisplay() {
         addTransactionTable.style.display = 'table';
         transactionHistoryVisibilityTable.style.display = 'none';
         varsVisibilityTable.style.display = 'none';
+        statsVisibilityTable.style.display = 'none';
         addTransactionTableState = 1;
         transactionHistoryTableState = 0;
         varsTableState = 0;
+        statsTableState = 0;
         console.log('ran 1')
         amountInputBox.focus();
     }

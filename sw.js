e7920ff5458578448704d8da92274f301797247a
santanderelias <cazagb@gmail.com>
2025-11-17 18:51:30 -0300

    small tweaks

--- sw.js
+++ sw.js
@@ -22,7 +22,7 @@ self.addEventListener('install', event => {
     caches.open(CACHE_NAME)
       .then(cache => {
         console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+        return Promise.all(urlsToCache.map(url => cache.add(url)));
       })
   );
 });
